1d3b2e0922899ae1325cb9ba2c152d6c6247d2c2
Vijay Lakhujani <lakhujanivijay@gmail.com>
2018-05-10 10:26:49 +0000

    Update Asgn_1.R

--- Statistics_and_R/Week1/Null Distributions Exercises/Asgn_1.R
+++ Statistics_and_R/Week1/Null Distributions Exercises/Asgn_1.R
@@ -1,3 +1,17 @@
+#####For these exercises, we will be using the following dataset:
+
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) )
+
+Here x represents the weights for the entire population.
+Null Distributions Exercises #1
+1/1 point (graded)
+
+Set the seed at 1, then using a for-loop take a random sample of 5 mice 1,000 times. Save these averages. What proportion of these 1,000 averages are more than 1 gram away from the average of x ? 
+
 library(downloader) 
 url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
 filename <- basename(url)
